28500e11e6205e18ad254934eeab169386a6d159
jf-bitt <52838384+jf-bitt@users.noreply.github.com>
2020-01-16 15:21:33 +0000

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,29 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+
 import numpy as np
-from matplotlib import pyplot as plt
+import pandas as pd
+
+data = pd.read_csv(r"C:\Users\joseph.foley\Downloads\linear_data.csv")
+
+x = np.array(data[['x1','x2','x3']])
+y = np.array(data[['y']])
+
+
+inter_ = np.array([np.ones(len(y))])
+
+
+x = np.append(inter_.T, x, axis=1)
 
 
 class LinearRegression:
+    
+    
+    def __init__(self,X_train, y_train, max_iter=1000, lr=0.0001):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.max_iter = max_iter
+        self.lr = lr
 
-    def __init__(self, X, y):
-        self.X = X
-        self.y = y
 
     def fit(self, X_train, y_train):
-        coefficients = self.gradient_descent(X_train, y_train, theta=np.zeros([len(X_train[0]), 1]), lr=0.001, max_iter=1000)
+        empty_theta=np.zeros([len(X_train[0])+1, 1])
+        coefficients = self.gradient_descent(X_train, y_train, theta=empty_theta)
         return coefficients
 
-    def gradient_descent(self, X_train, y_train, theta, lr=0.001, max_iter=1000):
+    def gradient_descent(self, X_train, y_train, theta):
         n = len(y_train)
-        cost_history = np.zeros(max_iter)
-        theta_history = np.zeros((max_iter, len(X_train[0])))
+        cost_history = np.zeros(self.max_iter)
+        theta_history = np.zeros((self.max_iter, len(X_train[0])+1))
+        
+        inter_ = np.array([np.ones(n)])
 
-        for i in range(max_iter):
+        X_train = np.append(inter_.T, X_train, axis=1)
+
+        for i in range(self.max_iter):
             y_pred = np.dot(X_train, theta)
-
-            theta = theta - (1 / n) * lr * (X_train.T.dot((y_pred - y_train)))
+            
+            theta = theta - (self.lr/ n)*(np.dot(X_train.T,y_pred-np.array([y_train]).T))
+            
             theta_history[i, :] = theta.T
 
-            cost = (1 / (2 * n)) * np.sum(np.square(y_pred - y_train))
+            cost = (1 / (2 * n)) * np.sum(np.square(y_pred - np.array([y_train]).T))
             cost_history[i] = cost
 
             # if abs((1 / n) * lr * (X_train.T.dot((y_pred - y_train)))).any() < 10**-2:
@@ -31,27 +61,29 @@ class LinearRegression:
             #     print((1 / n) * lr * (X_train.T.dot((y_pred - y_train))))
             #     break
 
-        # print("cost history = {}, theta = {}, predicted y = {} "
-        #       .format(cost_history, theta, y_pred))
+        #print("theta = {}"
+        #      .format(theta))
 
         return theta
 
     def predict(self, X_test):
-        i = 0
         predictions = []
-        coef = self.fit(self.X, self.y)
+        inter_ = np.array([np.ones(len(X_test))])
+        coef = self.fit(self.X_train, self.y_train)
+        X_test = np.append(inter_.T, X_test, axis=1)
         for value in X_test:
-            predictions.append((np.dot(value, coef))[0])
+            predictions.append((np.dot(value, coef)))
+        print(predictions)
         return predictions
 
 
-x = np.array([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]])
-y = np.array([[15], [30]])
 
+x = np.array(data[['x1','x2','x3']])
+y = np.array(data['y'])
+
+x2 = np.array([[10, 10, 10],[20,20,20]])
 lr = LinearRegression(x, y)
 
 coef = lr.fit(x, y)
 
-y_pred = lr.predict(x)
-
-print(y_pred)
+predict = lr.predict(x2)
